303552c517aa96c80f05d770c4985ca56fcd20ed
thesteffVM <theodorsteff@gmail.com>
2025-11-09 22:27:43 +0200

    minor corrections

--- tests/test_automationexercise.py
+++ tests/test_automationexercise.py
@@ -1,5 +1,5 @@
 def test_google_title(driver):
-    """Open google.com and check the page title contains 'Google'."""
+    """Open automationexercise.com and check the page title contains 'Automation'."""
     driver.get("https://automationexercise.com/")
     # Some regions may show localized titles; we assert 'Google' is a substring.
     assert "Automation" in driver.title
