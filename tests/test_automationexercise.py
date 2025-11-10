fb22540ad00fc6c8e523dc0c36480de1961df43e
thesteffVM <theodorsteff@gmail.com>
2025-11-10 14:30:56 +0200

    updated jenkis docker container file

--- tests/test_automationexercise.py
+++ tests/test_automationexercise.py
@@ -1,4 +1,4 @@
-def test_google_title(driver):
+def test_site_title(driver):
     """Open automationexercise.com and check the page title contains 'Automation'."""
     driver.get("https://automationexercise.com/")
     assert "Automation" in driver.title
